05a6ebd932a56047f075e6c8a92c98e4457a0fbc
maumau613081 <maumau613081@gmail.com>
2026-02-28 22:51:24 +0900

    update .js

--- feb28.js
+++ feb28.js
@@ -1,1 +1,13 @@
+const rows = 10;
+const cols = 10;
+const board = [];
 
+for (let i = 1; i <= rows; i++) {
+    const row = [];
+    for (let j = 1; j <= cols; j++) {
+        row.push(`x:${j}, y:${i}`);
+    }
+    board.push(row);
+}
+
+console.log(board);

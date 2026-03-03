02d5d8ed885c933ffedac35ed4294245245d957f
maumau613081 <maumau613081@gmail.com>
2026-03-03 20:00:39 +0900

    update main

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
-const ranges = { };
+class DrawCanvas {
+    constructor (cellSize) {
+        const canvas = document.getElementById('puzzleCanvas');
+        this.cellSize = cellSize;
+        ctx = canvas.getContext('2d');
+    }
+
+    drawBoard () {
+        ctx.beginPath();
+        ctx.strokeStyle = "#000000"
+        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+        for (let i = 1; i < gridSize; i++) {
+            ctx.moveTo(cellSize * i, 0);
+            ctx.lineTo(cellSize * i, canvas.height);
+            ctx.moveTo(0, cellSize * i);
+            ctx.lineTo(canvas.width, cellSize * i);
+        }
+        ctx.strokeStyle = "#b1b1b1";
+        ctx.stroke();
+    }
+
+    resetCanvas () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBoard();
+    }
+
+    refreshCanvas (regions, regionItems) {
+        this.resetCanvas();
+        for (const region in regions) {
+            for (const cell in region) {
+                if (cell.x, cell.y) {
+                    ctx.beginPath();
+                    ctx.strokeStyle = "#000000"
+                    ctx.moveTo(cellSize * (cell.x + 1));
+                    ctx.lineTo();
+                    ctx.stroke();
+                }
+            }
+        }
+    }
+}
+
 const gridSize = 10;
 const canvas = document.getElementById('puzzleCanvas');
 const ctx = canvas.getContext('2d');
@@ -10,27 +51,12 @@ const puzzle = new StarBattlePuzzleImport(gridSize, gridSize);
 puzzle.addRegions(ranges);
 const solver = new StarBattlePuzzleSolver(gridSize, gridSize, puzzle.board, puzzle.regions);
 
-function drawOutline() {
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-}
-
-function drawCells() {
-    ctx.beginPath();
-    for (let i = 1; i < gridSize; i++) {
-        ctx.moveTo(cellSize * i, 0);
-        ctx.lineTo(cellSize * i, canvas.height);
-        ctx.moveTo(0, cellSize * i);
-        ctx.lineTo(canvas.width, cellSize * i);
-    }
-    ctx.strokeStyle = "#b1b1b1";
-    ctx.stroke();
-}
-
 canvas.addEventListener('click', (event) => {
     const rect = canvas.getBoundingClientRect();
     const cursor = {x : event.clientX - rect.left, y : event.clientY -rect.top};
     const col = Math.floor(cursor.x / cellSize);
     const row = Math.floor(cursor.y / cellSize);
+    if (regions.come(region => region.some(pos => pos.x === col && pos.y === row))) return;
     regionItems.push({x : col, y : row})
     ctx.beginPath();
     ctx.fillStyle = "#ffbbbb";
@@ -60,7 +86,6 @@ document.getElementById('resetBtn').addEventListener('click', () => {
     drawOutline();
     drawCells();
 })
-
 
 drawOutline();
 drawCells();
